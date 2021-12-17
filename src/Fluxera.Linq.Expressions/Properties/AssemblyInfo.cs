40be4b05b0a33595b8ddca7bcd652eff2fc5b5c0
Matthias Gernand <matthias.gernand@fluxera.com>
2021-12-17 20:03:03 +0100

    Fixed versioning.

--- src/Fluxera.Linq.Expressions/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Fluxera.Linq.Expressions")]
-[assembly: AssemblyDescription("A collection of LINQ expression utilities and extension methods.")]
